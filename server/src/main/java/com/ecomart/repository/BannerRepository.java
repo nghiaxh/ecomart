1461c38929822ea9750a5fd0d2a9b69d29c6d1c3
Nghia Hoang <nghiahoang880@gmail.com>
2026-09-01 22:56:51 +0700

    feat: add by-name lookups for seed idempotency

--- server/src/main/java/com/ecomart/repository/BannerRepository.java
+++ server/src/main/java/com/ecomart/repository/BannerRepository.java
@@ -4,7 +4,9 @@ import com.ecomart.domain.entity.Banner;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BannerRepository extends JpaRepository<Banner, Long> {
     List<Banner> findByIsActiveTrueOrderByDisplayOrderAsc();
+    Optional<Banner> findByTitle(String title);
 }
--- server/src/main/java/com/ecomart/repository/MaterialRepository.java
+++ server/src/main/java/com/ecomart/repository/MaterialRepository.java
@@ -3,5 +3,8 @@ package com.ecomart.repository;
 import com.ecomart.domain.entity.Material;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MaterialRepository extends JpaRepository<Material, Long> {
+    Optional<Material> findByName(String name);
 }
