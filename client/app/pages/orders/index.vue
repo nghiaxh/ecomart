8c185e797ca481c350febecd49f11c1fd34c7886
Nghia Hoang <nghiahoang880@gmail.com>
2026-09-01 22:58:37 +0700

    refactor: restrict order pages to customers

--- client/app/pages/orders/index.vue
+++ client/app/pages/orders/index.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import type { Order, PageResponse } from '~/types'
 
-definePageMeta({ middleware: 'auth' })
+definePageMeta({ middleware: 'customer' })
 
 const { request } = useApi()
 const { formatVND, formatDate } = useFormat()
